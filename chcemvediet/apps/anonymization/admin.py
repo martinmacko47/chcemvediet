e180e14f90f58f69cefc21bee009a048f459bc78
Viliam Balaz <balaz@sited.sk>
2019-08-28 21:40:37 +0200

    #186 Anonymize attachments to pdf (#213)
    * #186 Anonymize attachments to pdf
    
    * #203 Create AttachmentFinalization model
    
    * #186 Review

--- chcemvediet/apps/anonymization/admin.py
+++ chcemvediet/apps/anonymization/admin.py
@@ -7,7 +7,8 @@ from poleno.utils.misc import decorate, filesize
 from poleno.utils.admin import admin_obj_format
 from poleno.attachments.views import download
 
-from .models import AttachmentNormalization, AttachmentRecognition, AttachmentAnonymization
+from .models import (AttachmentNormalization, AttachmentRecognition, AttachmentAnonymization,
+                     AttachmentFinalization)
 
 
 @admin.register(AttachmentNormalization, site=admin.site)
@@ -201,3 +202,67 @@ class AttachmentAnonymizationAdmin(admin.ModelAdmin):
                 url(r'^(.+)/download/$', download_view, name=u'{}_{}_download'.format(*info)),
                 )
         return urls + super(AttachmentAnonymizationAdmin, self).get_urls()
+
+@admin.register(AttachmentFinalization, site=admin.site)
+class AttachmentFinalizationAdmin(admin.ModelAdmin):
+    date_hierarchy = u'created'
+    list_display = [
+            u'id',
+            decorate(
+                lambda o: admin_obj_format(o.attachment, u'{obj}'),
+                short_description=u'Attachment',
+                admin_order_field=u'attachment',
+            ),
+            u'successful',
+            decorate(
+                lambda o: admin_obj_format(o, u'{obj.file.name}', link=u'download'),
+                short_description=u'File',
+                admin_order_field=u'file',
+                ),
+            u'name',
+            u'content_type',
+            u'created',
+            decorate(
+                lambda o: filesize(o.size),
+                short_description=u'Size',
+                admin_order_field=u'size',
+                ),
+            ]
+    list_filter = [
+            u'created',
+            u'content_type',
+            u'successful',
+            ]
+    search_fields = [
+            u'=id',
+            u'file',
+            u'name',
+            u'content_type',
+            u'debug',
+            ]
+    ordering = [
+            u'-id',
+            ]
+    exclude = [
+            u'file',
+            ]
+    readonly_fields = [
+            ]
+    raw_id_fields = [
+            ]
+    inlines = [
+            ]
+
+    def download_view(self, request, attachment_finalization_pk):
+        attachment_finalization = AttachmentFinalization.objects.get_or_404(
+            pk=attachment_finalization_pk
+        )
+        return download(request, attachment_finalization)
+
+    def get_urls(self):
+        info = self.model._meta.app_label, self.model._meta.model_name
+        download_view = self.admin_site.admin_view(self.download_view)
+        urls = patterns('',
+                url(r'^(.+)/download/$', download_view, name=u'{}_{}_download'.format(*info)),
+                )
+        return urls + super(AttachmentFinalizationAdmin, self).get_urls()
--- chcemvediet/apps/anonymization/cron.py
+++ chcemvediet/apps/anonymization/cron.py
@@ -3,6 +3,7 @@ from poleno.cron import cron_job
 from .normalization import normalize_attachment
 from .recognition import recognize_attachment
 from .anonymization import anonymize_attachment
+from .finalization import finalize_attachment
 
 
 @cron_job(run_every_mins=1)
@@ -10,3 +11,4 @@ def anonymization():
     normalize_attachment()
     recognize_attachment()
     anonymize_attachment()
+    finalize_attachment()
