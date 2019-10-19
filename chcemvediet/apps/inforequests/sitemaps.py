8928a4eae3910230bf4e15d2679fb1e3d3f32c86
Viliam Balaz <viliambalaz@gmail.com>
2019-10-19 14:18:50 +0200

    #210 Update inforequests sitemap and add no results alert

--- chcemvediet/apps/inforequests/sitemaps.py
+++ chcemvediet/apps/inforequests/sitemaps.py
@@ -1,11 +1,14 @@
 from django.conf import settings
+from django.core.paginator import Paginator
 from django.core.urlresolvers import reverse
 from django.contrib.sitemaps import Sitemap
 from django.db.models import Max
+from django.utils.http import urlencode
 
 from poleno.utils.urls import reverse
 from poleno.utils.translation import translation
 
+from .constants import INFOREQUESTS_PER_PAGE
 from .models import Action, Inforequest
 
 
@@ -26,3 +29,16 @@ class InforequestsSitemap(Sitemap):
         return (Action.objects
                 .of_inforequest(inforequest)
                 .aggregate(most_recent=Max(u'created'))[u'most_recent'])
+
+class PublishedInforequestsSitemap(Sitemap):
+    changefreq = u'weekly'
+    priority = 0.3
+
+    def items(self):
+        published_inforequests = Inforequest.objects.published()
+        paginator = Paginator(published_inforequests, INFOREQUESTS_PER_PAGE)
+        return [(lang, i) for lang, name in settings.LANGUAGES for i in paginator.page_range]
+
+    def location(self, (lang, i)):
+        with translation(lang):
+            return reverse(u'inforequests:index') + u'?' + urlencode({u'page': i})
--- chcemvediet/sitemaps.py
+++ chcemvediet/sitemaps.py
@@ -1,11 +1,12 @@
 from poleno.pages.sitemaps import PagesSitemap
 
-from .apps.inforequests.sitemaps import InforequestsSitemap
+from .apps.inforequests.sitemaps import InforequestsSitemap, PublishedInforequestsSitemap
 from .apps.obligees.sitemaps import ObligeesSitemap
 
 
 sitemaps = {
         u'pages': PagesSitemap,
         u'inforequests': InforequestsSitemap,
+        u'published_inforequests': PublishedInforequestsSitemap,
         u'obligees': ObligeesSitemap,
 }
