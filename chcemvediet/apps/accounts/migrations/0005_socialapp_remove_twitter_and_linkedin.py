c11d6a0a7b4482053ed6fc8494f777a22ab613cd
viliambalaz <balaz@sited.sk>
2020-06-08 11:02:02 +0200

    #241 Delete related SocialAccounts

--- chcemvediet/apps/accounts/migrations/0005_socialapp_remove_twitter_and_linkedin.py
+++ chcemvediet/apps/accounts/migrations/0005_socialapp_remove_twitter_and_linkedin.py
@@ -1,19 +1,23 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+from django.contrib.sites.models import Site
 from django.db import models, migrations
 
 
 def forward(apps, schema_editor):
     SocialApp = apps.get_model(u'socialaccount', u'SocialApp')
-    SocialApp.objects.filter(pk__in=[3, 4]).delete()
+    SocialAccount = apps.get_model(u'socialaccount', u'SocialAccount')
+    SocialApp.objects.filter(provider__in=[u'twitter', 'linkedin']).delete()
+    SocialAccount.objects.filter(provider__in=[u'twitter', u'linkedin']).delete()
 
 def backward(apps, schema_editor):
     SocialApp = apps.get_model(u'socialaccount', u'SocialApp')
-    twitter = SocialApp.objects.create(pk=3, provider=u'twitter', name=u'Twitter OAuth')
-    twitter.sites.add(1)
-    linkedin = SocialApp.objects.create(pk=4, provider=u'linkedin', name=u'Linkedin OAuth')
-    linkedin.sites.add(1)
+    site = Site.objects.get_current()
+    twitter = SocialApp.objects.create(provider=u'twitter', name=u'Twitter OAuth')
+    twitter.sites.add(site.pk)
+    linkedin = SocialApp.objects.create(provider=u'linkedin', name=u'Linkedin OAuth')
+    linkedin.sites.add(site.pk)
 
 class Migration(migrations.Migration):
 
