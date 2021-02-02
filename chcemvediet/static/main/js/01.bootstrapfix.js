ceb43d461f818906d4dbb184907376928fbd708c
viliambalaz <balaz@sited.sk>
2021-02-02 19:37:35 +0100

    #337 Collapse faq questions

--- chcemvediet/static/main/js/01.bootstrapfix.js
+++ chcemvediet/static/main/js/01.bootstrapfix.js
@@ -8,4 +8,12 @@ $(function(){
 	$('body').on('click', '[data-hide]', function(event){
 		$(this).closest('.' + $(this).attr('data-hide')).hide();
 	});
+
+    // Change icon on collapse
+    $('.collapse').on('shown.bs.collapse', function () {
+        $(this).parent().find(".chv-icon").removeClass("icon-plus").addClass("icon-minus");
+    });
+    $('.collapse').on('hidden.bs.collapse', function () {
+        $(this).parent().find(".chv-icon").removeClass("icon-minus").addClass("icon-plus");
+    });
 });
